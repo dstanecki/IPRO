35d58172910292db1045493a3c150ff50b14109d
dstanecki <45369450+dstanecki@users.noreply.github.com>
2021-03-30 18:43:57 -0500

    Add files via upload
    Added code to allow for pausing of extension.

--- background.js
+++ background.js
@@ -4,3 +4,16 @@ chrome.runtime.onInstalled.addListener(() => {
   chrome.storage.sync.set({ color });
   console.log('Default background color set to %cgreen', `color: ${color}`);
 });
+
+var isExtensionOn = true;
+
+chrome.extension.onMessage.addListener(
+function (request, sender, sendResponse) {
+	if (request.cmd == "setOnOffState") {
+		isExtensionOn = request.data.value;
+	}
+	
+	if (request.cmd == "getOnOffState") {
+		sendResponse(isExtensionOn);
+	}
+});
--- contentScript.js
+++ contentScript.js
@@ -1,22 +1,19 @@
 const words = document.querySelectorAll('h1, h2, h3, h4, h5, h6, p, li, td, caption, span, a');
-//let string='TAG';
-let div=document.createElement('div');
-div.id='tag';
-let text=documrnt.createTextNode('TAG');
+
 for(let i = 0; i < words.length; i++){
     if(words[i].innerHTML.includes('COVID-19')|| words[i].innerHTML.includes('covid-19')){
-        div.appendChild(text);
+
     } else if(words[i].innerHTML.includes('COVID19') || words[i].innerHTML.includes('covid19') || words[i].innerHTML.includes('Covid19')){
-        div.appendChild(text);
+
     } else if(words[i].innerHTML.includes('Coronavirus') || words[i].innerHTML.includes('coronavirus') || words[i].innerHTML.includes('Corona Virus') || words[i].innerHTML.includes('Corona virus') || words[i].innerHTML.includes('corona virus')){
-       div.appendChild(text);
+
     } else if(words[i].innerHTML.includes('Moderna Vaccine') || words[i].innerHTML.includes('Moderna vaccine') || words[i].innerHTML.includes('moderna vaccine')){
-       div.appendChild(text);
+
     } else if(words[i].innerHTML.includes('Pfizer Vaccine') || words[i].innerHTML.includes('Pfizer vaccine') || words[i].innerHTML.includes('pfizer vaccine')){
-       div.appendChild(text);
+
     } else if(words[i].innerHTML.includes('Johnson & Johnson Vaccine') || words[i].innerHTML.includes('Johnson & Johnson vaccine') || words[i].innerHTML.includes('J&J vaccine')){
-       div.appendChild(text);
+
     } else if(words[i].innerHTML.includes('Quarantine') || words[i].innerHTML.includes('quarantine') || words[i].innerHTML.includes('Self-Quarantine') || words[i].innerHTML.includes('Self-quarantine') || words[i].innerHTML.includes('self-quarantine')){
-       div.appendChild(text);
+
     }
 }
