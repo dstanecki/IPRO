d4e89a216af2036fe6b923ac6747c2fc91872365
am8triple9 <46827154+am8triple9@users.noreply.github.com>
2021-04-13 16:45:05 -0500

    Add files via upload

--- background.js
+++ background.js
@@ -4,16 +4,3 @@ chrome.runtime.onInstalled.addListener(() => {
   chrome.storage.sync.set({ color });
   console.log('Default background color set to %cgreen', `color: ${color}`);
 });
-
-var isExtensionOn = true;
-
-chrome.extension.onMessage.addListener(
-function (request, sender, sendResponse) {
-	if (request.cmd == "setOnOffState") {
-		isExtensionOn = request.data.value;
-	}
-	
-	if (request.cmd == "getOnOffState") {
-		sendResponse(isExtensionOn);
-	}
-});
---
+++ popup.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMCONTENTLOADED', function(){
+    var getInfoTag = document.getElementById('clickIt');
+    getInfoTag.addEventListener('click', function(){
+        chrome.tabs.getSelected(null, function(tab){
+            alert("This is a list of credible sources for Covid-19 related news.");
+        });
+    }, false);
+}, false);
