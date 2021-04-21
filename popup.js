e921b58b24318e1e624a77210afd991c3387c7f3
am8triple9 <46827154+am8triple9@users.noreply.github.com>
2021-04-20 19:10:27 -0500

    Added CDC alert

--- popup.js
+++ popup.js
@@ -3,6 +3,7 @@ document.addEventListener('DOMCONTENTLOADED', function(){
     getInfoTag.addEventListener('click', function(){
         chrome.tabs.getSelected(null, function(tab){
             alert("This is a list of credible sources for Covid-19 related news.");
+            alert("https://www.cdc.gov/");
         });
     }, false);
 }, false);
